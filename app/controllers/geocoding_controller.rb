9777538ae591538d72729314b2ccdfe47fee5777
krling13 <kirsten.ling@yale.edu>
2015-10-16 00:19:56 -0500

    hw3_1

--- app/controllers/geocoding_controller.rb
+++ app/controllers/geocoding_controller.rb
@@ -17,13 +17,13 @@ class GeocodingController < ApplicationController
     #   characters removed, is in the string url_safe_street_address.
     # ==========================================================================
 
-    url="http://maps.googleapis.com/maps/api/geocode/json?address=" + "#{url_safe_street_address}"
+    url="http://maps.googleapis.com/maps/api/geocode/json?address="+"#{url_safe_street_address}"
     open(url)
     raw_data=open(url).read
     require 'json'
     parsed_data = JSON.parse(raw_data)
-    @latitude = @parsed_data["results"][0]["geometry"]["location"]["lat"]
-    @longitude = @parsed_data["results"][0]["geometry"]["location"]["lng"]
+    @latitude = parsed_data["results"][0]["geometry"]["location"]["lat"]
+    @longitude = parsed_data["results"][0]["geometry"]["location"]["lng"]
 
     render("street_to_coords.html.erb")
   end
